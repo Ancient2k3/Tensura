f26ac0a864aa3d9d9f44daa12659f9e96ab16ed8
DE <trinhro2022@gmail.com>
2026-05-11 11:05:03 +0700

    Update SailorPiece.lua

--- SailorPiece.lua
+++ SailorPiece.lua
@@ -20,7 +20,7 @@ function receive_enm_pos()
     for _, npc in pairs(npcs:GetChildren()) do
       if npc and npc:FindFirstChild("Humanoid") then
         local dist = (npc:GetBoundingBox().Position - hrp.Position).magnitude
-        if dist <= 200 and npc.Humanoid.Health > 0 then
+        if dist <= 1000 and npc.Humanoid.Health > 0 then
           table.insert(t, npc:GetBoundingBox().Position)
         end
       end
