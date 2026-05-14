e3cd0668491c17d1de27e0aa0783510b598a1346
DE <trinhro2022@gmail.com>
2026-05-14 19:17:51 +0700

    Update and rename TSB_AI to MODEL_E5.lua

--- TSB_MODULES/Auto-Battle:AI/MODEL_E5.lua
+++ TSB_MODULES/Auto-Battle:AI/MODEL_E5.lua
@@ -1,3 +1,4 @@
+-- Final Test for E Rank: 5 --
 local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoangHienXScripts/Scripts/refs/heads/main/modules/quickbuttons.lua"))()
 ui.set_configs({saving_state = false})
 
