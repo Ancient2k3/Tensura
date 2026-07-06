d7d8c4f89cf6a552f18329280b5c36d940b15c5c
DE <trinhro2022@gmail.com>
2026-07-06 14:07:27 +0700

    Implement chat response on ultimate activation
    Added functionality to trigger a chat response when the player communicates and has maximum ultimate.

--- TSB_MODULES/Auto-Battle:AI/MODEL_D4.lua
+++ TSB_MODULES/Auto-Battle:AI/MODEL_D4.lua
@@ -152,7 +152,9 @@ function _use_ult()
   local ult = tonumber(plr:GetAttribute("Ultimate")) == 100
   if comm and ult then
     comm:FireServer({Goal = "KeyPress", Key = Enum.KeyCode.G})
+    vars.chat_func = true
     _chat_str(({"It's your death!", "Sorry, i doesn't understand what ur saying...", "Err... or... ", "MuaHaHAHAHaha", "That hurt..."})[math.random(1, 5)]) -- fun chat when activated
+    vars.chat_func = false
   end
 end
 
